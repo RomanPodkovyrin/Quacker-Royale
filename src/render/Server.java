d0823a0d7b7022df5307e5755467514b50a2dcb0
axl620 <axl620@cs.bham.ac.uk>
2019-01-24 12:25:46 +0000

    server now can receive information from the client

---
+++ src/render/Server.java
@@ -0,0 +1,54 @@
+package render;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Server extends Thread {
+
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+
+    public Server() throws SocketException {
+        socket = new DatagramSocket(4445);
+    }
+
+    public void run() {
+        running = true;
+
+        while (running) {
+            DatagramPacket packet
+                    = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(buf, buf.length, address, port);
+            String received
+                    = new String(packet.getData(), 0, packet.getLength());
+            System.out.println(received);
+            if (received.equals("end")) {
+                running = false;
+                continue;
+            }
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
+    }
+
+    public static void main(String[] args) throws SocketException {
+        new Server().start();
+    }
+}
+
