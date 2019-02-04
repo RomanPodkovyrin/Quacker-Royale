9fce11a232a2bbcccac0c8f6bcdfd80d457e1f7a
alfi-s <alfisumadi@gmail.com>
2019-02-04 10:45:15 +0000

    GameSession and GameSessionController setup change

--- src/main/java/com/anotherworld/control/GameSessionController.java
+++ src/main/java/com/anotherworld/control/GameSessionController.java
@@ -1,34 +1,32 @@
 package com.anotherworld.control;
 
 import com.anotherworld.model.logic.GameSession;
+import com.anotherworld.model.movable.Player;
 import com.anotherworld.view.View;
 import com.anotherworld.view.input.KeyListener;
 
+import java.util.ArrayList;
+
 public class GameSessionController {
 
-    public static void main(String[] args) {
-        new GameSessionController();
-    }
-
-    private static boolean isRunning;
     private GameSession session;
     private View view;
     private Thread viewThread;
     private KeyListener keyListener;
 
-    public GameSessionController() {
+    public GameSessionController(View view, GameSession session) {
 
-        isRunning = true;
-        //this.session = new GameSession(null, null);
-        this.view = new View();
+        this.session = session;
+        this.view = view;
+
+        // Starting the View thread
         this.viewThread = new Thread(view);
         viewThread.start();
-        System.out.println("Thread started");
-        try{
-            Thread.sleep(1000);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        // Sleeping the main thread for 1 second to register the key inputs.
+        try { Thread.sleep(1000); }
+        catch (Exception e){ e.printStackTrace(); }
+
+        // Obtain the key listener.
         this.keyListener = view.getKeyListener();
         mainLoop();
 
@@ -59,6 +57,20 @@ public class GameSessionController {
     }
 
     private static void render() {
+        //View.update
+    }
 
+    public static void main(String[] args) {
+        //Create the players
+        Player ourPlayer = new Player("Us", 3,10,10,null, false);
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(ourPlayer);
+        ArrayList<Player> ais = new ArrayList<>();
+
+        //Create the game view and models
+        View gameView = new View();
+        GameSession gameSession = new GameSession(ourPlayer, null, null);
+
+        new GameSessionController(gameView, gameSession);
     }
 }
--- src/main/java/com/anotherworld/model/logic/GameSession.java
+++ src/main/java/com/anotherworld/model/logic/GameSession.java
@@ -6,24 +6,35 @@ import com.anotherworld.tools.PropertyReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * A class that models a game session.
+ * @author Alfi S.
+ */
 public class GameSession {
     private static PropertyReader properties;
     private static int numberOfBalls;
+    private Player currentPlayer;
     private ArrayList<Player> players;
     private ArrayList<Player> ais;
     private ArrayList<Ball> balls;
 
-    public GameSession(ArrayList<Player> players, ArrayList<Player> ais) {
+    public GameSession(Player currentPlayer,
+                       ArrayList<Player> players, ArrayList<Player> ais) {
+
+        // Receive the data from the properties file.
         try {
             this.properties = new PropertyReader("logic.properties");
             this.numberOfBalls = Integer.parseInt(properties.getValue("NUMBER_OF_BALLS"));
         } catch (IOException e) {
             System.err.println("Error when loading properties class: " + e.getMessage());
         }
+
+        this.currentPlayer = currentPlayer;
         this.players = players; //Create the list of players
         this.ais = ais;
+
         for(int i = 0; i < numberOfBalls; i++) {
-            //Instantiate a new ball at a random? location
+            //Instantiate a new ball at a random(?) location
         }
     }
 
@@ -32,6 +43,9 @@ public class GameSession {
      * physics and ai are run during this time
      */
     public void update(){
+        // Update the positions of the current player based on given input.
+
+        // Update the positions of the other players.
 
         // Check whether or not the players are within the arena.
 
