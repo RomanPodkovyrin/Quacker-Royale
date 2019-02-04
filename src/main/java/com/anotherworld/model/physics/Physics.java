ed8fd151e1268768e1c23bf01d9b422677be2dca
chk703 <chk703@student.bham.ac.uk>
2019-02-04 19:33:11 +0800

    removed unnecessary methods

--- src/main/java/com/anotherworld/model/physics/Physics.java
+++ src/main/java/com/anotherworld/model/physics/Physics.java
@@ -8,24 +8,11 @@ import com.anotherworld.model.movable.Ball;
 import com.anotherworld.model.movable.Player;
 
 public class Physics {
-    public float[] calculateXYVelocity(float speed, float angle) {
-        float[] xyVelocity = new float[2];
-        xyVelocity[0] = (float) (speed * Math.sin(angle));
-        xyVelocity[1] = (float) (speed * Math.cos(angle));
-        return xyVelocity;
-    }
 
-    public float[] calculateSpeedAngle(float xVelocity, float yVelocity) {
-        float[] speedAngle = new float[2];
-        speedAngle[0] = Math.abs(yVelocity * yVelocity + xVelocity * xVelocity);
-        speedAngle[1] = (float) Math.tanh(xVelocity / yVelocity);
-        return speedAngle;
-    }
-
-    public float[] move(float xVelocity, float yVelocity, float xCoordinate,
-            float yCoordinate) {
-        float[] coordinates = { xCoordinate + xVelocity, yCoordinate + yVelocity };
-        return coordinates;
+    public void move(AbstractMovable object) {
+        float newXCoordinate = object.getxCoordinate() + object.getxVelocity();
+        float newYCoordinate = object.getyCoordinate() + object.getyVelocity();
+        object.setCoordinates(newXCoordinate, newYCoordinate);
     }
 
     public boolean checkCollision(AbstractMovable a, AbstractMovable b) {
@@ -43,21 +30,20 @@ public class Physics {
         float circleR = a.getRadius();
         float circleX = a.getxCoordinate();
         float circleY = a.getyCoordinate();
-        float[] direction = {circleY-circleR, circleX+circleR, circleY+circleR, circleX-circleR};
-        if(direction[0] < wallCoordinate[0]) {
-            a.setCoordinates(a.getxCoordinate(), wallCoordinate[0]+circleR);
+        float[] direction = { circleY - circleR, circleX + circleR,
+                circleY + circleR, circleX - circleR };
+        if (direction[0] < wallCoordinate[0]) {
+            a.setCoordinates(a.getxCoordinate(), wallCoordinate[0] + circleR);
+            a.setyVelocity(-a.getyVelocity());
+        } else if (direction[2] > wallCoordinate[2]) {
+            a.setCoordinates(a.getxCoordinate(), wallCoordinate[2] - circleR);
             a.setyVelocity(-a.getyVelocity());
         }
-        else if(direction[2] > wallCoordinate[2]) {
-            a.setCoordinates(a.getxCoordinate(), wallCoordinate[2]-circleR);
-            a.setyVelocity(-a.getyVelocity());
-        }
-        if(direction[1]>wallCoordinate[1]){
-            a.setCoordinates(wallCoordinate[1]-circleR, a.getyCoordinate());
+        if (direction[1] > wallCoordinate[1]) {
+            a.setCoordinates(wallCoordinate[1] - circleR, a.getyCoordinate());
             a.setxVelocity(-a.getxVelocity());
-        }
-        else if(direction[3]<wallCoordinate[3]){
-            a.setCoordinates(wallCoordinate[3]+circleR, a.getyCoordinate());
+        } else if (direction[3] < wallCoordinate[3]) {
+            a.setCoordinates(wallCoordinate[3] + circleR, a.getyCoordinate());
             a.setxVelocity(-a.getxVelocity());
         }
     }
@@ -80,18 +66,11 @@ public class Physics {
         a.setSpeed(speed);
     }
 
-    public void forceCancelling(AbstractMovable a, float newSpeed,
-            float newAngle) {
-        float[] xyVelocity = calculateXYVelocity(newSpeed, newAngle);
+    public void forceCancelling(AbstractMovable a, float[] xyVelocity) {
         float xVelocity = a.getxVelocity();
         float yVelocity = a.getyVelocity();
         a.setxVelocity(xVelocity + xyVelocity[0]);
         a.setyVelocity(yVelocity + xyVelocity[1]);
-    }
-
-    public void collided(Ball ball) {
-        float velocityX = ball.getxVelocity();
-        ball.setxVelocity(velocityX * -1);
     }
 
     public void collided(Player player, float[] outsideVelocity) {
@@ -108,7 +87,8 @@ public class Physics {
         }
     }
 
-    public void onCollision(List<Ball> listOfBalls, List<Player> listOfPlayers, float[] wallDimensions) {
+    public void onCollision(List<Ball> listOfBalls, List<Player> listOfPlayers,
+            float[] wallDimensions) {
         // check for ball collided, then player collided.
         // The list of integer collided is for storing the index of player
         // To make sure the event has already occurred on them during this
