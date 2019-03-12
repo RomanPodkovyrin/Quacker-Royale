9d314822855ef5bcf2dc06116706ce9494ad876a
roman <rxp777@student.bham.ac.uk>
2019-03-12 14:48:04 +0000

    cleaded up player behaviour

--- src/main/java/com/anotherworld/model/ai/behaviour/player/AvoidNeutralPlayer.java
+++ src/main/java/com/anotherworld/model/ai/behaviour/player/AvoidNeutralPlayer.java
@@ -7,9 +7,8 @@ import com.anotherworld.model.ai.tools.MatrixMath;
 import com.anotherworld.model.logic.Platform;
 import com.anotherworld.model.movable.Ball;
 import com.anotherworld.model.movable.Player;
-import java.util.ArrayList;
-
 import com.anotherworld.tools.maths.Maths;
+import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -35,7 +34,10 @@ public class AvoidNeutralPlayer extends Job {
         this.platform = platform;
 
         for (Player player: players) {
+
             if (!player.isDead()) {
+
+                // Checks the distance between the player and the ai
                 if (MatrixMath.distanceAB(player.getCoordinates(), ai.getCoordinates()) <= player.getRadius() + ai.getRadius() + 0.5) {
                     fail();
                     logger.debug("Avoiding player " + player.getCharacterID());
@@ -43,8 +45,8 @@ public class AvoidNeutralPlayer extends Job {
                     Line line = new Line(player.getCoordinates(), vector);
                     Matrix orthogonal = line.getOrthogonalVector();
 
-                    ai.setXVelocity(Maths.floatDivision(orthogonal.getX() , Math.abs(orthogonal.getX())) * ai.getSpeed());
-                    ai.setYVelocity(Maths.floatDivision(orthogonal.getY() , Math.abs(orthogonal.getY())) * ai.getSpeed());
+                    ai.setXVelocity(Maths.floatDivision(orthogonal.getX(), Math.abs(orthogonal.getX())) * ai.getSpeed());
+                    ai.setYVelocity(Maths.floatDivision(orthogonal.getY(), Math.abs(orthogonal.getY())) * ai.getSpeed());
                     return;
                 }
             }
