f0884c12095d4f5ef9ae03e40c6a8b3bbcf63b44
roman <rxp777@student.bham.ac.uk>
2019-03-17 13:52:19 +0000

    checkstyle fix

--- src/main/java/com/anotherworld/audio/AudioControl.java
+++ src/main/java/com/anotherworld/audio/AudioControl.java
@@ -2,14 +2,23 @@ package com.anotherworld.audio;
 
 import java.io.IOException;
 
+/**
+ * This class controls background and sound effects.
+ *
+ * @author roman
+ * @author anton
+ */
 public class AudioControl {
-    private static BackgroundMusic backgroundMusic ;
+    private static BackgroundMusic backgroundMusic;
     private static SoundEffects soundEffects;
     private static boolean musicOn = true;
     private static boolean effectsOn = true;
     private static boolean mutedSoundEffects = false;
     private static boolean mutedBackgroundMusic = false;
 
+    /**
+     * To be used at the beginning of the game to start background and effects depending on the previous settings.
+     */
     public static void setUp() {
         if (musicOn) {
             backgroundMusic = new BackgroundMusic();
@@ -22,30 +31,50 @@ public class AudioControl {
         // soundEffects set up
     }
 
+    /**
+     * Sets background music to either on or off.
+     * @param musicOn true - on, false - off
+     */
     public static void setMusicOn(boolean musicOn) {
         AudioControl.musicOn = musicOn;
     }
 
+    /**
+     * Sets sound effects to either on or off.
+     * @param effectsOn true - on, false - off
+     */
     public static void setEffectsOn(boolean effectsOn) {
         AudioControl.effectsOn = effectsOn;
     }
 
+    /**
+     * Mutes of unmutes background music and sound effects.
+     */
     public static void muteUnmute(){
 
     }
 
+    /**
+     * Starts playing background music.
+     */
     public static void playBackGroundMusic() {
         if (musicOn) {
             backgroundMusic.playBackgroundMusic();
         }
     }
 
+    /**
+     * Stops playing background music.
+     */
     public static void stopBackgroundMusic() {
         if (musicOn) {
             backgroundMusic.terminateMusic();
         }
     }
 
+    /**
+     * Plays player collision with the ball.
+     */
     public static void playerCollidedWithBall() {
         if (effectsOn) {
 
@@ -53,12 +82,18 @@ public class AudioControl {
         }
     }
 
+    /**
+     * Plays ball collision with the wall.
+     */
     public static void ballCollidedWithWall() {
         if (effectsOn) {
             soundEffects.ballCollidedWithWall();
         }
     }
 
+    /**
+     * Stops all sound effects.
+     */
     public static void stopSoundEffects() {
         if (effectsOn) {
             soundEffects.stopSoundEffects();
