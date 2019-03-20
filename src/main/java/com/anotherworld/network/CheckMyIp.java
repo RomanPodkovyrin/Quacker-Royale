4b833d989bfdb54cdb3330aa61eb997cd3ae63a3
axl620 <axl620@cs.bham.ac.uk>
2019-03-20 15:30:19 +0000

    check my ip wip

--- src/main/java/com/anotherworld/network/CheckMyIp.java
+++ src/main/java/com/anotherworld/network/CheckMyIp.java
@@ -1,16 +1,21 @@
 package com.anotherworld.network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class CheckMyIp {
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress inetAddress = InetAddress.getLocalHost();
-        System.out.println("1: " + inetAddress.getHostAddress());
-        System.out.println("2: " + inetAddress.getCanonicalHostName());
-        System.out.println("3: " + Inet4Address.getLocalHost());
-        System.out.println("4: " + Inet6Address.getLocalHost());
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String macCommand = "ipconfig getifaddr en1";
+        System.out.println(execCmd(macCommand));
+    }
+    public static String execCmd(String cmd) throws java.io.IOException {
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
